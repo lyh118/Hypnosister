73868cfabaf24884a8d9eefa5fcc720780d1348e
N3962 <n3962@N3962ui-Mac-mini.local>
2015-04-02 15:25:31 +0900

    Chap4 Hypnosister First Commit

---
+++ COMMHyponosisView.h
@@ -0,0 +1,13 @@
+//
+//  COMMHyponosisView.h
+//  Hypnosister
+//
+//  Created by N3962 on 2015. 4. 2..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COMMHyponosisView : UIView
+
+@end
---
+++ COMMHyponosisView.m
@@ -0,0 +1,21 @@
+//
+//  COMMHyponosisView.m
+//  Hypnosister
+//
+//  Created by N3962 on 2015. 4. 2..
+//
+//
+
+#import "COMMHyponosisView.h"
+
+@implementation COMMHyponosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
